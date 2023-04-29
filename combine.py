dcf5630e2872fea5349b1bed3d6cedc7681af923
mkami2828 <67807605+mkami2828@users.noreply.github.com>
2023-04-29 13:51:16 +0500

    Create combine.py

---
+++ combine.py
@@ -0,0 +1,9 @@
+#Take two inputs from the user
+input1 = input("Enter the first input: ")
+input2 = input("Enter the second input: ")
+
+#combine te inputs
+combined_input = input1 + ipnut2
+
+#print the output
+print("The combined input is: ", combined_input)
